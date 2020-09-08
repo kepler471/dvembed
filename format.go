24f1cdd479e0ddaa1818548b2f1b3d95fd744d0b
Stelios Georgiou <steliosdg@gmail.com>
2020-09-08 22:13:27 +0100

    initial formatting args for ffmpeg, using webm

--- format.go
+++ format.go
@@ -1,7 +1,30 @@
 package main
 
-// Format handles file conversion and formatting with ffmpeg.
-// Discord allows a maximum 8MB file upload
-func Format() {
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path"
+)
 
+// Format handles file conversion and formatting with ffmpeg.
+// Discord allows a maximum 8MB file upload, so this is only
+// required if the size limit is exceeded.
+func Format(m Media) (Media, error) {
+	cmd := exec.Command(
+		"cmd",
+		"-i "+m.Info.Name(),
+		" -c:v libvpx ",
+		"-crf 0 ",
+		"-b:v 1M ",
+		"-c:a libvorbis ",
+		m.Id+ConvertedExt,
+	)
+	cmd.Dir = path.Join(Dir, m.Id)
+	err := cmd.Run()
+	if err != nil {
+		log.Fatalf("Could not run %v\n", cmd.Args)
+	}
+	m.Info, err = os.Stat(path.Join(cmd.Dir, m.Id+ConvertedExt))
+	return m, err
 }
