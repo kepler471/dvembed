32f63a1200b2c638bb10368778594a7bba378890
Stelios Georgiou <steliosdg@gmail.com>
2020-09-09 12:30:47 +0100

    build for multiple arch

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+package=$1
+major=$2
+minor=$3
+patch=$4
+
+if [[ -z "$package" || -z "$major" || -z "$minor" || -z "$patch" ]]; then
+  echo "usage: $0 <package-name> <major-ver-num> <patch-ver-num> <patch-ver-num>"
+  exit 1
+fi
+
+platforms=("windows/amd64" "linux/amd64" "darwin/amd64")
+zip=""
+
+for platform in "${platforms[@]}"
+do
+  platform_split=(${platform//\// })
+  GOOS=${platform_split[0]}
+  GOARCH=${platform_split[1]}
+
+  path=releases/v"$major"/"$minor"/"$patch"/"$GOOS"/"$package"
+
+  if [ $GOOS = "windows" ]; then
+    path+='.exe'
+  fi
+
+  env GOOS="$GOOS" GOARCH="$GOARCH" go build -o "$path" "$package"
+
+  if [ $? -ne 0 ]; then
+    echo 'An error has occurred! Aborting the script execution...'
+    exit 1
+  fi
+
+  zip+="$GOOS"" "
+done
+
+tar -czf releases/v"$major"/"$minor"/"$patch"/dvembed.tar.gz -C releases/v"$major"/"$minor"/"$patch" $zip
+#echo tar -czf releases/v"$major"/"$minor"/"$patch"/dvembed.tar.gz "$zip"
