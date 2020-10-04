b7ee7e159f9eee9bf189da159ee8b03f2b8d2662
Stelios Georgiou <18307819+kepler471@users.noreply.github.com>
2020-10-04 01:55:22 +0100

    check token
    added log.Fatal for missing token

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ const (
 
 // Run the bot client
 func main() {
+	if TOKEN == "" {
+		log.Fatal("Secret token (in token.go) is missing.\nThe token can be found at https://discord.com/developers/applications/")
 	err := os.MkdirAll(dir, 0755)
 	if err != nil {
 		log.Printf("Error creating base download directory: %v", err)
