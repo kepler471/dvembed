44c49fd93fae797b8dabe2cd8814ec29b303653d
Stelios Georgiou <steliosdg@gmail.com>
2021-03-10 00:30:02 +0000

    change renamed func

--- ffmpeg_test.go
+++ ffmpeg_test.go
@@ -19,13 +19,13 @@ func TestFormat(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := format(tt.args.m)
+			got, err := compress(tt.args.m)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("format() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("compress() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("format() got = %v, want %v", got, tt.want)
+				t.Errorf("compress() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
